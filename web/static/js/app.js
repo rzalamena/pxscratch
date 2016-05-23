1507ec7b8ff69657620a6349c3a26187ab3c6d6d
Rafael Zalamena <rzalamena@gmail.com>
2016-05-23 18:09:37 -0300

    Added tools for appearance
    Removed the stock code from Phoenix Framework and added a more standard one.
     * Added JQuery (needed by refills code);
     * Added SASS support;
     * Added normalize.css;
     * Added Bourbon, Bitters and Neat;
     * Replaced icons with icons from Aiconica that uses license CC0;

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,59 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+import $ from "jquery"
+
+//
+// pxscratch JS code
+//
+$(() => {
+  //
+  // Box close
+  //
+  let box_close_button = $(".box-close")
+  box_close_button.on("click", (e) => {
+    e.preventDefault()
+    e.toElement.parentNode.remove()
+  })
+})
+
+//
+// Refills code
+//
+
+//
+// header
+//
+$(window).resize(function() {
+  var more = document.getElementById("js-navigation-more");
+  if ($(more).length > 0) {
+    var windowWidth = $(window).width();
+    var moreLeftSideToPageLeftSide = $(more).offset().left;
+    var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
+
+    if (moreLeftSideToPageRightSide < 330) {
+      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-right");
+      $("#js-navigation-more .submenu .submenu").addClass("fly-out-left");
+    }
+
+    if (moreLeftSideToPageRightSide > 330) {
+      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-left");
+      $("#js-navigation-more .submenu .submenu").addClass("fly-out-right");
+    }
+  }
+});
+
+$(document).ready(function() {
+  var menuToggle = $("#js-mobile-menu").unbind();
+  $("#js-navigation-menu").removeClass("show");
+
+  menuToggle.on("click", function(e) {
+    e.preventDefault();
+    $("#js-navigation-menu").slideToggle(function(){
+      if($("#js-navigation-menu").is(":hidden")) {
+        $("#js-navigation-menu").removeAttr("style");
+      }
+    });
+  });
+});
