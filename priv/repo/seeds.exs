c7694c0890218a7b4d9daee59882a457700ac076
Rafael Zalamena <rzalamena@gmail.com>
2016-06-03 10:31:08 -0300

    Improve the seed file format
    Allow people to run seed file again without duplicating entries.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -18,57 +18,74 @@ alias Pxscratch.User
 #
 # default roles
 #
-%Role{}
-|> Role.changeset(%{
+create_role = fn(role) ->
+  old_role = Repo.get_by(Role, name: role.name)
+  if old_role do
+    old_role
+  else
+    role
+    |> Role.changeset(%{})
+    |> Repo.insert!
+  end
+end
+
+default_role = create_role.(%Role{
   name: "Default",
   description: "Default user role",
 })
-|> Repo.insert!
 
-
-role =
-  %Role{}
-  |> Role.changeset(%{
-    name: "Administrator",
-    description: "The website administrator",
-    admin: true
-  })
-  |> Repo.insert!
+admin_role = create_role.(%Role{
+  name: "Administrator",
+  description: "The website administrator",
+  admin: true
+})
 
 
 #
 # users
 #
-default_password = "abcdabcd"
+create_admin = fn(user, role) ->
+  default_password = "abcdabcd"
+  old_user = Repo.get_by(User, role_id: role.id)
+  if old_user do
+    old_user
+  else
+    user
+    |> User.changeset(%{role_id: role.id, password: default_password})
+    |> Repo.insert!
+  end
+end
 
-%User{}
-|> User.changeset(%{
+create_admin.(%User{
   nickname: "admin",
   email: "admin@admin.com",
-  password: default_password,
-  password_confirmation: default_password,
-  role_id: role.id
-})
-|> Repo.insert!
+}, admin_role)
 
 
 #
 # settings
 #
-%Setting{}
-|> Setting.changeset(%{
+create_setting = fn(setting) ->
+  old_setting = Repo.get_by(Setting, name: setting.name)
+  if old_setting do
+    old_setting
+  else
+    setting
+    |> Setting.changeset(%{})
+    |> Repo.insert!
+  end
+end
+
+create_setting.(%Setting{
   name: "website_name",
   description: "Website name",
   type: "text",
   tvalue: "Pxscratch",
 })
-|> Repo.insert!
 
-%Setting{}
-|> Setting.changeset(%{
+create_setting.(%Setting{
   name: "public_sign_in",
   description: "Allows anyone to sign in",
   type: "boolean",
   bvalue: false,
 })
-|> Repo.insert!
